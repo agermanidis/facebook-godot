8e6f31851d6993102b09db7953664a4c540e03c2
Iain Nash <iain@hioscar.com>
2017-02-25 17:50:00 -0500

    rm

--- js_api/waiting-godot.js
+++
@@ -1,9 +0,0 @@
-
-
-login({email: 'codejoust@gmail.com', password: 'iainnash'}, function(err, api) {
-  if (err) return console.error(err);
-  api.listen((err, message) => {
-   api.sendMessage(message.body, message.threadID);
-   console.log(message);
- });
-});
